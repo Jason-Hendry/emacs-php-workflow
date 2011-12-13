be67e3ef341c737b4826d1c882ecdb77f13091cb
Jason Hendry <jason+github.com@rain.com.au>
2011-12-13 11:21:19 +1100

    Add Auto Complete Add Smarty Mode alist Set open-resource directory when opening project

--- init.el
+++ init.el
@@ -1,10 +1,19 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (load "php-mode")
-(load "smarty-mode")
 (load "sr-speedbar")
+(require 'project-mode)
 (require 'open-resource)
+(require 'smarty-mode)
 (sr-speedbar-open)
 
+(project-mode 1)
+
 (global-set-key "\C-\M-r" 'open-resource)
-(setq open-resource-repository-directory "~/www")
 (setq open-resource-ignore-patterns (quote ("~$" ".svn" "templates_cpl")))
+
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "/Users/jasonhendry/.emacs.d/ac-dict")
+(ac-config-default)
+
+(add-to-list 'auto-mode-alist '("\\.tpl" . smarty-mode))
+(add-to-list 'auto-mode-alist '("\\.php" . php-mode))
